3f5c0aa4de8792656797f36b6a9a79460457ef24
İsmail <idincer944@gmail.com>
2024-05-11 18:43:31 +0300

    Added error handler for auth

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,15 +1,21 @@
-import {Request, Response} from 'express'
+import {NextFunction, Request, Response} from 'express'
 import { prismaClient } from '..';
 import {compareSync, hashSync} from 'bcrypt'
 import * as jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '../secrets';
+import { BadRequestsException } from '../exceptions/bad-request';
+import { ErrorCode } from '../exceptions/root';
+import { UnprocessableEntity } from '../exceptions/validation';
+import { SignUpSchema } from '../schema/users';
+import { NotFoundException } from '../exceptions/not-found';
 
-export const signup = async (req:Request, res:Response) => {
+export const signup = async (req:Request, res:Response, next: NextFunction) => {
+    SignUpSchema.parse(req.body)
     const {email, password, name} = req.body;
 
     let user = await prismaClient.user.findFirst({where: {email}})
     if (user) {
-        throw Error('User already exists')
+        throw new BadRequestsException('User already exists', ErrorCode.USER_ALREADY_EXISTS)
     }
 
     user = await prismaClient.user.create({
@@ -27,10 +33,10 @@ export const login = async (req:Request, res:Response) => {
 
     let user = await prismaClient.user.findFirst({where: {email}})
     if (!user) {
-        throw Error('User does not exists')
+        throw new NotFoundException('User not found', ErrorCode.USER_NOT_FOUND)
     }
     if(!compareSync(password, user.password)){
-        throw Error('Incorrect email or password')
+        throw new BadRequestsException('Incorrect password', ErrorCode.INCORRECT_PASSWORD)
     }
     const token = jwt.sign({userId: user.id}, JWT_SECRET)
 
--- src/exceptions/root.ts
+++ src/exceptions/root.ts
@@ -1,14 +1,12 @@
 // message, status code, error codes, error
 
-import { AnyTxtRecord } from "dns";
-
-class HttpException extends Error {
+export class HttpException extends Error {
     message: string;
     errorCode: any;
     statusCode: number;
-    errors: any;
+    errors: ErrorCode;
 
-    constructor(message: string, errorCode: any, statusCode: number, errors: any) {
+    constructor(message: string, errorCode: ErrorCode, statusCode: number, errors: any) {
         super(message);
         this.message = message;
         this.errorCode = errorCode;
@@ -17,8 +15,10 @@ class HttpException extends Error {
     }
 }
 
-export enum ErrorCodes {
+export enum ErrorCode {
     USER_NOT_FOUND = 1001,
     USER_ALREADY_EXISTS = 1002,
-    INVALID_CREDENTIALS = 1003,
+    INCORRECT_PASSWORD = 1003,
+    UNPROCESSABLE_ENTITY = 2001,
+    INTERNAL_EXCEPTION = 3001
 }
