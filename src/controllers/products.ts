6f281ce4651b7c18ce5906b615f3ab2656603891
İsmail <idincer944@gmail.com>
2024-05-13 15:58:25 +0300

    Added crud for products

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { prismaClient } from "..";
+import { NotFoundException } from "../exceptions/not-found";
+import { ErrorCode } from "../exceptions/root";
 
 export const createProduct = async(req: Request, res: Response) => {
     const product = await prismaClient.product.create({
@@ -10,4 +12,63 @@ export const createProduct = async(req: Request, res: Response) => {
     })
 
     res.json(product);
+}
+
+export const updateProduct = async(req: Request, res: Response) => {
+    try {
+        const product = req.body
+        if(product.tags){
+            product.tags = product.tags.join(', ')
+        }
+
+        const updateProduct = await prismaClient.product.update({
+            where: {
+                id: +req.params.id
+            },
+            data: product
+        })
+        res.json(updateProduct);
+    } catch (error) {
+        throw new NotFoundException('Product not found', ErrorCode.PRODUCT_NOT_FOUND)
+    }
+}
+
+export const deleteProduct = async(req: Request, res: Response) => {
+    try {
+        await prismaClient.product.delete({
+            where: {
+                id: +req.params.id
+            }
+        })
+        res.json({message: 'Product deleted'})
+    } catch (error) {
+        throw new NotFoundException('Product not found', ErrorCode.PRODUCT_NOT_FOUND)
+    }
+}
+
+export const listProducts = async(req: Request, res: Response) => {
+    
+    const count = await prismaClient.product.count()
+    const skip = req.query.skip ? +req.query.skip : 0; 
+    const products = await prismaClient.product.findMany({
+        skip: skip,
+        take: 5
+    })
+    res.json({
+        count, data:products
+    })
+}
+
+export const getProductById = async(req: Request, res: Response) => {
+    try {
+        const product = await prismaClient.product.findFirstOrThrow({
+            where: {
+                id: +req.params.id
+            }
+        })
+        res.json(product)
+    } catch (error) {
+        throw new NotFoundException('Product not found', ErrorCode.PRODUCT_NOT_FOUND)
+        
+    }
 }
